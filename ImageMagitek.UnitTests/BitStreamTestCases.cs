3d11f314e5dfc965764a62e325e12703a9ebabf2
Stephen Monaco <stevemonaco@hotmail.com>
2019-06-25 19:05:55 -0400

    Added tests for BitStream read methods

---
+++ ImageMagitek.UnitTests/BitStreamTestCases.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using ImageMagitek;
+
+namespace ImageMagitek.UnitTests
+{
+    public class BitStreamTestCases
+    {
+        public static byte[] readData = new byte[] { 0b10110011, 0b11111111, 0b01010101, 0b11001100, 0b00000001 };
+
+        public static IEnumerable<TestCaseData> ReadBitCases
+        {
+            get
+            {
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 0, 1);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 1, 0);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 2, 1);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 7, 1);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 16, 0);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 39, 1);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> ReadByteCases
+        {
+            get
+            {
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 0, (byte)0b10110011);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 1, (byte)0b01100111);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 2, (byte)0b11001111);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 3, (byte)0b10011111);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 7, (byte)0b11111111);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 16, (byte)0b01010101);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 32, (byte)0b00000001);
+            }
+        }
+
+        public static IEnumerable<TestCaseData> ReadBitsCases
+        {
+            get
+            {
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 0, 1, 1);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 0, 2, 2);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 8, 8, 0b11111111);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 10, 8, 0b11111101);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 19, 18, 0b101011100110000000);
+                yield return new TestCaseData(BitStream.OpenRead(readData, readData.Length * 8), 16, 24, 0b010101011100110000000001);
+            }
+        }
+    }
+}
---
+++ ImageMagitek.UnitTests/BitStreamTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using ImageMagitek;
+using NUnit.Framework;
+
+namespace ImageMagitek.UnitTests
+{
+    [TestFixture]
+    public class BitStreamTests
+    {
+        [TestCaseSource(typeof(BitStreamTestCases), "ReadBitCases")]
+        public void ReadBit_ReturnsValue(BitStream stream, int bitSeek, int expected)
+        {
+            for (int i = 0; i < bitSeek; i++)
+                stream.ReadBit();
+
+            var actual = stream.ReadBit();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(typeof(BitStreamTestCases), "ReadByteCases")]
+        public void ReadByte_ReturnsValue(BitStream stream, int bitSeek, byte expected)
+        {
+            for (int i = 0; i < bitSeek; i++)
+                stream.ReadBit();
+
+            var actual = stream.ReadByte();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCaseSource(typeof(BitStreamTestCases), "ReadBitsCases")]
+        public void Readits_ReturnsValue(BitStream stream, int bitSeek, int bitReadSize, int expected)
+        {
+            for (int i = 0; i < bitSeek; i++)
+                stream.ReadBit();
+
+            var actual = stream.ReadBits(bitReadSize);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
