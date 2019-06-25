c7fba99856a6c1ba95d34b6971c1269f32e4e7e7
Stephen Monaco <stevemonaco@hotmail.com>
2019-06-25 14:24:52 -0400

    Split BitStream-supporting code into individual files

---
+++ ImageMagitek/ExtensionMethods/FileExtensionMethods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ImageMagitek.ExtensionMethods
+{
+    /// <summary>
+    /// Adds additional methods to FileStream related to bitwise file reading
+    /// </summary>
+    public static class FileExtensionMethods
+    {
+        public static byte[] ReadUnshifted(this FileStream file, FileBitAddress address, int numBits, bool Seek)
+        {
+            if (Seek)
+                file.Seek(address.FileOffset, SeekOrigin.Begin);
+
+            using (BinaryReader br = new BinaryReader(file, new UTF8Encoding(), true))
+            {
+                if (address.BitOffset == 0 && (numBits % 8 == 0)) // Byte-aligned, whole-byte read
+                    return br.ReadBytes(numBits / 8);
+                else if (address.BitOffset != 0) // Byte-unaligned read
+                {
+                    int numBytes = (numBits + address.BitOffset + 7) / 8; // Add 7 to take advantage of integer truncation instead of using doubles and Math.Ceiling
+
+                    byte[] retArray = br.ReadBytes(numBytes);
+                    byte premaskbits = (byte)((1 << (8 - address.BitOffset)) - 1);
+                    retArray[0] &= premaskbits;
+                    byte postmaskbits = (byte)(premaskbits ^ 0xFF);
+                    retArray[numBytes - 1] &= postmaskbits;
+
+                    return br.ReadBytes(numBytes);
+                }
+                else
+                    throw new Exception();
+            }
+        }
+
+        /*public static byte[] ReadShifted(this FileStream file, FileBitAddress address, int numBits, bool Seek)
+        {
+            if(Seek)
+                file.Seek(address.FileOffset, SeekOrigin.Begin);
+
+            using (BinaryReader br = new BinaryReader(file, new UTF8Encoding(), true))
+            {
+                if (address.BitOffset == 0) // Byte-aligned, whole-byte read
+                    return br.ReadBytes(numBits / 8);
+            }
+        } */
+    }
+}
---
+++ ImageMagitek/FileBitAddress.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageMagitek
+{
+    /// <summary>
+    /// Struct used to store a file address that does not start on a byte-aligned address
+    /// </summary>
+    public struct FileBitAddress
+    {
+        /// <summary>
+        /// File offset in bytes
+        /// </summary>
+        public long FileOffset { get; set; }
+
+        /// <summary>
+        /// Number of bits to skip after FileOffset
+        /// Valid range is 0-7 inclusive
+        /// A zero value would result in a byte-aligned address
+        /// </summary>
+        public int BitOffset { get; set; }
+
+        public FileBitAddress(long fileOffset, int bitOffset)
+        {
+            if (bitOffset > 7)
+                throw new ArgumentOutOfRangeException($"{nameof(FileBitAddress)}: {nameof(bitOffset)} {bitOffset} is out of range");
+
+            FileOffset = fileOffset;
+            BitOffset = bitOffset;
+        }
+
+        /// <summary>
+        /// Construct a new FileBitAddress from the number of bits to the address
+        /// </summary>
+        /// <param name="bits"></param>
+        public FileBitAddress(long bits)
+        {
+            FileOffset = bits / 8;
+            BitOffset = (int)(bits % 8);
+        }
+
+        public long Bits()
+        {
+            return FileOffset * 8 + BitOffset;
+        }
+
+        /// <summary>
+        /// Casts a long into a FileBitAddress
+        /// </summary>
+        /// <param name="Address">Number of bits</param>
+        public static implicit operator FileBitAddress(long Address)
+        {
+            return new FileBitAddress(Address / 8, (int)(Address % 8));
+        }
+
+        /// <summary>
+        /// Adds two FileBitAddress objects and returns the result
+        /// </summary>
+        /// <param name="Address1"></param>
+        /// <param name="Address2"></param>
+        /// <returns></returns>
+        public static FileBitAddress operator +(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            long bits = Address1.Bits() + Address2.Bits();
+            return new FileBitAddress(bits);
+        }
+
+        /// <summary>
+        /// Adds a specified number of bits to a FileBitAddress object
+        /// </summary>
+        /// <param name="Address"></param>
+        /// <param name="Offset">Number of bits to advance the address</param>
+        /// <returns></returns>
+        public static FileBitAddress operator +(FileBitAddress Address, long Offset)
+        {
+            long bits = Address.Bits() + Offset;
+            return new FileBitAddress(bits);
+        }
+
+        /// <summary>
+        /// Subtracts two FileBitAddress objects and returns the result
+        /// </summary>
+        /// <param name="Address1"></param>
+        /// <param name="Address2"></param>
+        /// <returns></returns>
+        public static FileBitAddress operator -(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            long bits = Address1.Bits() - Address2.Bits();
+
+            return new FileBitAddress(bits);
+        }
+
+        /// <summary>
+        /// Subtracts a number of bits from a FileBitAddress object
+        /// </summary>
+        /// <param name="Address"></param>
+        /// <param name="Offset"></param>
+        /// <returns></returns>
+        public static FileBitAddress operator -(FileBitAddress Address, long Offset)
+        {
+            long addressbits = Address.Bits();
+            long retbits = addressbits - Offset;
+
+            return new FileBitAddress(retbits);
+        }
+
+        public static bool operator <(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            return Address1.Bits() < Address2.Bits();
+        }
+
+        public static bool operator <=(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            return Address1.Bits() <= Address2.Bits();
+        }
+
+        public static bool operator >(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            return Address1.Bits() > Address2.Bits();
+        }
+        public static bool operator >=(FileBitAddress Address1, FileBitAddress Address2)
+        {
+            return Address1.Bits() >= Address2.Bits();
+        }
+    }
+}
