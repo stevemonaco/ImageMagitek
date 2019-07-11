c2f847313cd2d96e032a481682bf2818a269eb9d
Stephen Monaco <stevemonaco@hotmail.com>
2019-07-10 23:59:10 -0400

    Removed unused DeserializeProject method

--- ImageMagitek/Project/XmlGameDescriptorWriter.cs
+++ ImageMagitek/Project/XmlGameDescriptorWriter.cs
@@ -203,29 +203,5 @@ namespace ImageMagitek.Project
 
             return arrangerNode;
         }
-
-
-        /*
-        public static void SerializeProject(Dictionary<string, ProjectResourceBase> projectTree, Stream stream)
-        {
-            if (projectTree is null)
-                throw new ArgumentNullException($"SerializeProject was called with a null {nameof(projectTree)}");
-            if (stream is null)
-                throw new ArgumentNullException("SerializeProject was called with a null stream");
-            if (!stream.CanWrite)
-                throw new ArgumentException("SerializeProject was called with a stream without write access");
-
-            var xmlRoot = new XElement("gdf");
-            var projectRoot = new XElement("project");
-            var settingsRoot = new XElement("settings");
-
-            xmlRoot.Add(settingsRoot);
-            xmlRoot.Add(projectRoot);
-
-            var orderedNodes = projectTree.Values.OrderBy(x => x, new ProjectResourceBaseComparer()).Where(x => x.ShouldBeSerialized);
-            orderedNodes.ForEach(x => projectRoot.Add(x.Serialize()));
-
-            xmlRoot.Save(stream);
-        } */
     }
 }
