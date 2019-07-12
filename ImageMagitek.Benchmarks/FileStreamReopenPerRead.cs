754ee25f9a742a3a0c74a120f315cc2f2ab12f85
Stephen Monaco <stevemonaco@hotmail.com>
2019-07-12 00:42:59 -0400

    Add benchmarking project

---
+++ ImageMagitek.Benchmarks/FileStreamReopenPerRead.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ImageMagitek.Benchmarks
+{
+    /// <summary>
+    /// Benchmark to determine performace impact of continually reopening FileStream objects contained by DataFiles
+    /// </summary>
+    public class FileStreamReopenPerRead
+    {
+        [Params(1000)]
+        public int N;
+
+        [Params(16, 64, 256, 512, 16384)]
+        public int TotalReadSize;
+
+        private const int SizePerRead = 16;
+        private const string fileName = "FileStreamReopenPerReadTestData.bin";
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            using (var fs = File.Create(fileName))
+            {
+                Random rng = new Random();
+                var data = new byte[16384];
+                rng.NextBytes(data);
+                fs.Write(data);
+            }
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            File.Delete(fileName);
+        }
+
+        [Benchmark]
+        public void KeepOpen()
+        {
+            using(var fs = File.OpenRead(fileName))
+            using(var br = new BinaryReader(fs))
+            {
+                for(int i = 0; i < TotalReadSize; i += SizePerRead)
+                {
+                    fs.Seek(i, SeekOrigin.Begin);
+                    br.ReadBytes(SizePerRead);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void ReopenPerRead()
+        {
+            for (int i = 0; i < TotalReadSize; i += SizePerRead)
+            {
+                using (var fs = File.OpenRead(fileName))
+                using (var br = new BinaryReader(fs))
+                {
+                    fs.Seek(i, SeekOrigin.Begin);
+                    br.ReadBytes(SizePerRead);
+                }
+            }
+        }
+    }
+}
---
+++ ImageMagitek.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ImageMagitek.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(FileStreamReopenPerRead));
+        }
+    }
+}
