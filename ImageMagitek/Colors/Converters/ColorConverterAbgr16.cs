630aa44266ad8829020e126b1aa25f362e08d679
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-04 23:28:48 -0400

    Added private opacity settings

--- ImageMagitek/Colors/Converters/ColorConverterAbgr16.cs
+++ ImageMagitek/Colors/Converters/ColorConverterAbgr16.cs
@@ -5,14 +5,18 @@ using System.Text;
 
 namespace ImageMagitek.Colors
 {
-    public class ColorConverterAbgr16 : IColorConverter<ColorAbgr16>
+    public sealed class ColorConverterAbgr16 : IColorConverter<ColorAbgr16>
     {
+        private const byte AlphaTransparent = 0;
+        private const byte AlphaSemiTransparent = 128;
+        private const byte AlphaOpaque = 255;
+
         public ColorAbgr16 ToForeignColor(ColorRgba32 nc)
         {
             byte r = (byte)(nc.r >> 3);
             byte b = (byte)(nc.b >> 3);
             byte g = (byte)(nc.g >> 3);
-            byte a = (byte)(nc.a == 0 ? 0 : 1);
+            byte a = (byte)(nc.a <= AlphaSemiTransparent ? 0 : 1);
 
             return new ColorAbgr16(r, g, b, a);
         }
@@ -22,7 +26,7 @@ namespace ImageMagitek.Colors
             byte r = (byte)(fc.r << 3);
             byte g = (byte)(fc.g << 3);
             byte b = (byte)(fc.b << 3);
-            byte a = (byte)(fc.a == 0 ? 0 : 0xFF);
+            byte a = fc.a == 0 ? AlphaTransparent : AlphaOpaque;
 
             return new ColorRgba32(r, g, b, a);
         }
