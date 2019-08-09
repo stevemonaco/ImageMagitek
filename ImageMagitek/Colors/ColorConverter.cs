916548d3ad067129876278464bd6bda742da4235
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-09 14:04:41 -0400

    Added explicit ABGR16 conversion rather than via ColorVector

--- ImageMagitek/Colors/ColorConverter.cs
+++ ImageMagitek/Colors/ColorConverter.cs
@@ -8,6 +8,7 @@ namespace ImageMagitek.Colors
     public static class ColorConverter
     {
         public static ColorConverterBgr15 Bgr15 { get; } = new ColorConverterBgr15();
+        public static ColorConverterAbgr16 Abgr16 { get; } = new ColorConverterAbgr16();
 
         public static ColorRgba32 ToNative(IColor32 color)
         {
@@ -15,6 +16,8 @@ namespace ImageMagitek.Colors
             {
                 case ColorBgr15 colorBgr15:
                     return Bgr15.ToNativeColor(colorBgr15);
+                case ColorAbgr16 colorAbgr16:
+                    return Abgr16.ToNativeColor(colorAbgr16);
                 case ColorRgba32 _:
                     return new ColorRgba32(color.Color);
                 default:
@@ -30,12 +33,14 @@ namespace ImageMagitek.Colors
             {
                 case ColorModel.BGR15:
                     return Bgr15.ToForeignColor(color);
+                case ColorModel.ABGR16:
+                    return Abgr16.ToForeignColor(color);
                 case ColorModel.RGB24:
                     throw new NotImplementedException();
                 case ColorModel.ARGB32:
                     throw new NotImplementedException();
-                case ColorModel.ABGR16:
-                    throw new NotImplementedException();
+                case ColorModel.RGBA32:
+                    return new ColorRgba32(color.Color);
                 case ColorModel.RGB15:
                     throw new NotImplementedException();
                 case ColorModel.NES:
