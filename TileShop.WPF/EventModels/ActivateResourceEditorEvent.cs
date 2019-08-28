6e65792ede42a286d36f425d80de43d45a605f41
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-28 09:54:36 -0400

    Add event to activate editors

---
+++ TileShop.WPF/EventModels/ActivateResourceEditorEvent.cs
@@ -0,0 +1,17 @@
+﻿using ImageMagitek.Project;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.WPF.EventModels
+{
+    public class ActivateResourceEditorEvent
+    {
+        public IProjectResource Resource { get; set; }
+
+        public ActivateResourceEditorEvent(IProjectResource resource)
+        {
+            Resource = resource;
+        }
+    }
+}
