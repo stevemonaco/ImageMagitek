a9172c502f13e8929fdde3ce9d5678c4a0be72fc
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-28 16:56:00 -0400

    Initial placeholder commit

---
+++ TileShop.WPF/ViewModels/Editors/DataFileEditorViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.WPF.ViewModels
+{
+    public class DataFileEditorViewModel : EditorBaseViewModel
+    {
+    }
+}
---
+++ TileShop.WPF/Views/Editors/DataFileEditorView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TileShop.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for DataFileEditorView.xaml
+    /// </summary>
+    public partial class DataFileEditorView : UserControl
+    {
+        public DataFileEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
