c25b66c3d6af873671cfd37c19afd712ca8060f9
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-28 16:59:09 -0400

    Initial commit for status bar notifications

---
+++ TileShop.WPF/EventModels/NotifyStatusEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.WPF.EventModels
+{
+    public enum NotifyStatusDuration { Short, Indefinite }
+    public class NotifyStatusEvent
+    {
+        public string NotifyMessage { get; set; }
+
+        public NotifyStatusDuration DisplayDuration { get; set; }
+
+        public NotifyStatusEvent(string message, NotifyStatusDuration displayDuration)
+        {
+            NotifyMessage = message;
+            DisplayDuration = displayDuration;
+        }
+    }
+}
