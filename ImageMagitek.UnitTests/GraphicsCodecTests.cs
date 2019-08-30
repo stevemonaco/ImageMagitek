f85ea2e2fa18aacdb98ae2225df00a9bfb4473d1
Stephen Monaco <stevemonaco@hotmail.com>
2019-08-29 23:14:29 -0400

    Added tests to be implemented later

---
+++ ImageMagitek.UnitTests/GraphicsCodecTests.cs
@@ -0,0 +1,14 @@
+﻿using ImageMagitek.Codec;
+using NUnit.Framework;
+
+namespace ImageMagitek.UnitTests
+{
+    [TestFixture]
+    public class GraphicsCodecTests
+    {
+        public void EncodeDecode_Reverses_AsExpected(IGraphicsCodec codec)
+        {
+
+        }
+    }
+}
