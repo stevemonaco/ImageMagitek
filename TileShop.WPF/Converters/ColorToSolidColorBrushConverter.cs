855f96054fdf9f67486fb1f1e2471a76648592da
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-03 17:32:41 -0400

    Initial commit

---
+++ TileShop.WPF/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TileShop.WPF.Converters
+{
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return null;
+
+            if (value is Color color)
+                return new SolidColorBrush(color);
+
+            throw new InvalidOperationException($"{nameof(ColorToSolidColorBrushConverter)}.{nameof(Convert)} cannot convert from given type {value.GetType()}");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
