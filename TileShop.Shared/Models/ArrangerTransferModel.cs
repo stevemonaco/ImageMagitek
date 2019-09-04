ecf889b4b8655ca5e9b610323ac3412593701d1c
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-04 00:44:38 -0400

    Renamed

--- TileShop.Shared/Models/ArrangerTransferModel.cs
+++ TileShop.Shared/Models/ArrangerTransferModel.cs
@@ -6,15 +6,27 @@ using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Text;
 
-namespace TileShop.WPF.Models
+namespace TileShop.Shared.Models
 {
     public class ArrangerTransferModel
     {
         public Arranger Arranger { get; set; }
 
+        /// <summary>
+        /// Left edge of arranger in pixels
+        /// </summary>
         public int X { get; set; }
+        /// <summary>
+        /// Left edge of arranger in pixels
+        /// </summary>
         public int Y { get; set; }
+        /// <summary>
+        /// Width of arranger in pixels
+        /// </summary>
         public int Width { get; set; }
+        /// <summary>
+        /// Height of arranger in pixels
+        /// </summary>
         public int Height { get; set; }
 
         public ArrangerTransferModel() { }
