76f418bf30752ba13ca279a8a295fb998a920cd3
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-04 00:43:28 -0400

    Added new event models

---
+++ TileShop.Shared/EventModels/EditArrangerPixelsEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TileShop.Shared.Models;
+
+namespace TileShop.Shared.EventModels
+{
+    public class EditArrangerPixelsEvent
+    {
+        public ArrangerTransferModel ArrangerTransferModel { get; set; }
+
+        public EditArrangerPixelsEvent(ArrangerTransferModel model)
+        {
+            ArrangerTransferModel = model;
+        }
+    }
+}
