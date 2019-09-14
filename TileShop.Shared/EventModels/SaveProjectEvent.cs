fe3ac277a1af325cc45bcf13c237953a81d32837
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-14 11:09:11 -0400

    Added event model for saving the project

---
+++ TileShop.Shared/EventModels/SaveProjectEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.Shared.EventModels
+{
+    public class SaveProjectEvent
+    {
+        public string ProjectFileName { get; set; }
+    }
+}
