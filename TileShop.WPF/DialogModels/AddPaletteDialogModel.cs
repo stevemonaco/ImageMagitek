6887ebf6a57fe0e09ef6f16bf6e960a3989a1605
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-14 11:13:01 -0400

    Added preliminary dialog models

---
+++ TileShop.WPF/DialogModels/AddPaletteDialogModel.cs
@@ -0,0 +1,61 @@
+﻿using Caliburn.Micro;
+using ImageMagitek;
+using ImageMagitek.Colors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.WPF.DialogModels
+{
+    public class AddPaletteDialogModel : PropertyChangedBase
+    {
+        private string _paletteName;
+        public string PaletteName
+        {
+            get => _paletteName;
+            set => Set(ref _paletteName, value);
+        }
+
+        private BindableCollection<DataFile> _dataFiles;
+        public BindableCollection<DataFile> DataFile
+        {
+            get => _dataFiles;
+            set => Set(ref _dataFiles, value);
+        }
+
+        private string _selectedDataFile;
+        public string SelectedDataFile
+        {
+            get => _selectedDataFile;
+            set => Set(ref _selectedDataFile, value);
+        }
+
+        private BindableCollection<ColorModel> _colorModels;
+        public BindableCollection<ColorModel> ColorModels
+        {
+            get => _colorModels;
+            set => Set(ref _colorModels, value);
+        }
+
+        private ColorModel _selectedColorModel;
+        public ColorModel SelectedColorModel
+        {
+            get => _selectedColorModel;
+            set => Set(ref _selectedColorModel, value);
+        }
+
+        private int _entries;
+        public int Entries
+        {
+            get => _entries;
+            set => Set(ref _entries, value);
+        }
+
+        private int _fileOffset;
+        public int FileOffset
+        {
+            get => _fileOffset;
+            set => Set(ref _fileOffset, value);
+        }
+    }
+}
---
+++ TileShop.WPF/DialogModels/AddTiledScatteredArrangerDialogModel.cs
@@ -0,0 +1,42 @@
+﻿using Caliburn.Micro;
+
+namespace TileShop.WPF.DialogModels
+{
+    public class AddTiledScatteredArrangerDialogModel : PropertyChangedBase
+    {
+        private string _arrangerName;
+        public string ArrangerName
+        {
+            get => _arrangerName;
+            set => Set(ref _arrangerName, value);
+        }
+
+        private int _arrangerElementWidth;
+        public int ArrangerElementWidth
+        {
+            get => _arrangerElementWidth;
+            set => Set(ref _arrangerElementWidth, value);
+        }
+
+        private int _arrangerElementHeight;
+        public int ArrangerElementHeight
+        {
+            get => _arrangerElementHeight;
+            set => Set(ref _arrangerElementHeight, value);
+        }
+
+        private int _elementPixelWidth;
+        public int ElementPixelWidth
+        {
+            get => _elementPixelWidth;
+            set => Set(ref _elementPixelWidth, value);
+        }
+
+        private int _elementPixelHeight;
+        public int ElementPixelHeight
+        {
+            get => _elementPixelHeight;
+            set => Set(ref _elementPixelHeight, value);
+        }
+    }
+}
