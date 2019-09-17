bb0246fbe105fb6782b539eab786a7cbbb3f5978
Stephen Monaco <stevemonaco@hotmail.com>
2019-09-17 17:23:00 -0400

    initial commit

---
+++ TileShop.Shared/EventModels/SaveEditorEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileShop.Shared.EventModels
+{
+    public class SaveEditorEvent
+    {
+        public bool PromptUser { get; set; }
+
+        public SaveEditorEvent() { }
+
+        public SaveEditorEvent(bool promptUser)
+        {
+            PromptUser = promptUser;
+        }
+    }
+}
