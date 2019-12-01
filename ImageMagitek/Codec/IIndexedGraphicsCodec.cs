55d07848d3096a276ff1a419a2a32d978e064b79
Stephen Monaco <stevemonaco@hotmail.com>
2019-11-30 19:31:36 -0500

    Fixed potential Dispose bug

---
+++ ImageMagitek/Codec/IIndexedGraphicsCodec.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageMagitek.Codec
+{
+    public interface IIndexedGraphicsCodec : IGraphicsCodec
+    {
+        void Decode(IndexedImage image, ArrangerElement el);
+        void Encode(IndexedImage image, ArrangerElement el);
+    }
+}
---
+++ ImageMagitek/IndexedImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageMagitek
+{
+    public class IndexedImage
+    {
+        public byte[,] Image { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public IndexedImage(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Image = new byte[Width, Height];
+        }
+
+        /// <summary>
+        /// Gets the palette-indexed color at the specified coordinate
+        /// </summary>
+        /// <param name="x">x-coordinate</param>
+        /// <param name="y">y-coordinate</param>
+        /// <returns>Palette-indexed color</returns>
+        public byte GetPixel(int x, int y)
+        {
+            if (Image is null)
+                throw new NullReferenceException($"{nameof(GetPixel)} property '{nameof(Image)}' was null");
+
+            return Image[x, y];
+        }
+
+        /// <summary>
+        /// Sets the specified coordinate of the image to a specified palette-indexed color
+        /// </summary>
+        /// <param name="x">x-coordinate of pixel</param>
+        /// <param name="y">y-coordinate of pixel</param>
+        /// <param name="color">Palette-indexed color to set</param>
+        public void SetPixel(int x, int y, byte color)
+        {
+            if (Image is null)
+                throw new NullReferenceException($"{nameof(SetPixel)} property '{nameof(Image)}' was null");
+
+            Image[x, y] = color;
+        }
+    }
+}
