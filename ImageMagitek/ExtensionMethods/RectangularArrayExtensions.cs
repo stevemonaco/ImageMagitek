92aa119f57ed96a04fdae8b4da184e97d1a195e5
Stephen Monaco <stevemonaco@hotmail.com>
2020-01-31 08:05:34 -0500

    Added utility extension methods for 2D array copying

--- ImageMagitek/ExtensionMethods/RectangularArrayExtensions.cs
+++ ImageMagitek/ExtensionMethods/RectangularArrayExtensions.cs
@@ -19,5 +19,33 @@ namespace ImageMagitek.ExtensionMethods
             foreach (T item in source)
                 yield return item;
         }
+
+        public static T[,] ToSubArray<T>(this T[,] source, int x0, int y0, int width, int height)
+        {
+            var subArray = new T[width, height];
+            source.CopyToArray(subArray, x0, y0, width, height);
+            return subArray;
+        }
+
+        public static void CopyToArray<T>(this T[,] source, T[,] dest, int x0, int y0, int width, int height)
+        {
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    dest[x, y] = source[x + x0, y + y0];
+        }
+
+        public static T[,] To2DArray<T>(this T[] source, int sourceX, int sourceY, int sourceWidth, int width, int height)
+        {
+            var array = new T[width, height];
+            source.CopyToArray(array, sourceX, sourceY, sourceWidth, width, height);
+            return array;
+        }
+
+        public static void CopyToArray<T>(this T[] source, T[,] dest, int sourceX, int sourceY, int sourceWidth, int destWidth, int destHeight)
+        {
+            for (int y = 0; y < destHeight; y++)
+                for (int x = 0; x < destWidth; x++)
+                    dest[x, y] = source[(y + sourceY) * sourceWidth + x + sourceX];
+        }
     }
 }
