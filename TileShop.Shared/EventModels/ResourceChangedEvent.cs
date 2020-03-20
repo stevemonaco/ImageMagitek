27993d7c3eb7eae08168906d70f7c0f3082fa8d1
Stephen Monaco <stevemonaco@hotmail.com>
2020-03-20 19:51:37 -0400

    Initial commit

---
+++ TileShop.Shared/EventModels/ResourceChangedEvent.cs
@@ -0,0 +1,18 @@
+﻿using ImageMagitek.Project;
+
+namespace TileShop.Shared.EventModels
+{
+    public enum ResourceChangeEffect { Project, File }
+
+    public class ResourceChangedEvent
+    {
+        public IProjectResource Resource { get; }
+        public ResourceChangeEffect Effect { get; }
+
+        public ResourceChangedEvent(IProjectResource resource, ResourceChangeEffect effect)
+        {
+            Resource = resource;
+            Effect = effect;
+        }
+    }
+}
