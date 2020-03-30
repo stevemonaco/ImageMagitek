4ac1860c9f7f891599d02ccd1b961f2d49b471b7
Stephen Monaco <stevemonaco@hotmail.com>
2020-03-30 00:12:35 -0400

    Hide arranger size for single layouts in AddScatteredArranger.

---
+++ TileShop.WPF/Converters/ArrangerLayoutToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using ImageMagitek;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TileShop.WPF.Converters
+{
+    public class ArrangerLayoutToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var parameterString = parameter as string;
+
+            if (value is ArrangerLayout state && Enum.TryParse(parameterString, out ArrangerLayout stateVisibility))
+            {
+                var visibility = (state, stateVisibility) switch
+                {
+                    (ArrangerLayout.Tiled, ArrangerLayout.Tiled) => Visibility.Visible,
+                    (ArrangerLayout.Single, ArrangerLayout.Single) => Visibility.Visible,
+                    _ => Visibility.Collapsed
+                };
+
+                return visibility;
+            }
+
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
