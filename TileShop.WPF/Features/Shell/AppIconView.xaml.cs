2ee73f9ab5406da21c194570b6cec2a6ef8555a1
Stephen Monaco <stevemonaco@hotmail.com>
2020-09-18 01:06:47 -0400

    Merged titlebar and menu. Added concept logo.

---
+++ TileShop.WPF/Features/Shell/AppIconView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TileShop.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for AppIconView.xaml
+    /// </summary>
+    public partial class AppIconView : UserControl
+    {
+        public AppIconView()
+        {
+            InitializeComponent();
+        }
+    }
+}
