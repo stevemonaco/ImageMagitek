0deac90a6ee31717c10c1f7246237ce088f31920
Stephen Monaco <stevemonaco@hotmail.com>
2020-11-01 01:00:53 -0400

    Add unit tests for pattern codecs

--- ImageMagitek.UnitTests/PatternListTestCases.cs
+++ ImageMagitek.UnitTests/PatternListTestCases.cs
@@ -1,31 +1,139 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
 using NUnit.Framework;
+using PC = ImageMagitek.Codec.PlaneCoordinate;
 
 namespace ImageMagitek.Codec.UnitTests
 {
     public class PatternListTestCases
     {
-        public static IEnumerable<TestCaseData> TryCreateRemapPatternTestCases
+        public static IEnumerable<TestCaseData> TryCreateRemapPlanarPatternTestCases
         {
             get
             {
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAACCCCCCCCBBBBBBBB" }, 24,
-                    new[] { 0, 1, 2, 3, 4, 5, 6, 7, 16, 17, 18, 19, 20, 21, 22, 23, 8, 9, 10, 11, 12, 13, 14, 15 }
+                    new[] { "AAAAAAAACCCCCCCCBBBBBBBB" }, 24, 4, 1, 24,
+                    new[]
+                    {
+                        new PC(0, 0, 0), new PC(1, 0, 0), new PC(2, 0, 0), new PC(3, 0, 0), new PC(4, 0, 0), new PC(5, 0, 0), new PC(6, 0, 0), new PC(7, 0, 0), new PC(16, 0, 0), new PC(17, 0, 0), new PC(18, 0, 0), new PC(19, 0, 0), new PC(20, 0, 0), new PC(21, 0, 0), new PC(22, 0, 0), new PC(23, 0, 0), new PC(8, 0, 0), new PC(9, 0, 0), new PC(10, 0, 0), new PC(11, 0, 0), new PC(12, 0, 0), new PC(13, 0, 0), new PC(14, 0, 0), new PC(15, 0, 0),
+                        new PC(0, 1, 0), new PC(1, 1, 0), new PC(2, 1, 0), new PC(3, 1, 0), new PC(4, 1, 0), new PC(5, 1, 0), new PC(6, 1, 0), new PC(7, 1, 0), new PC(16, 1, 0), new PC(17, 1, 0), new PC(18, 1, 0), new PC(19, 1, 0), new PC(20, 1, 0), new PC(21, 1, 0), new PC(22, 1, 0), new PC(23, 1, 0), new PC(8, 1, 0), new PC(9, 1, 0), new PC(10, 1, 0), new PC(11, 1, 0), new PC(12, 1, 0), new PC(13, 1, 0), new PC(14, 1, 0), new PC(15, 1, 0),
+                        new PC(0, 2, 0), new PC(1, 2, 0), new PC(2, 2, 0), new PC(3, 2, 0), new PC(4, 2, 0), new PC(5, 2, 0), new PC(6, 2, 0), new PC(7, 2, 0), new PC(16, 2, 0), new PC(17, 2, 0), new PC(18, 2, 0), new PC(19, 2, 0), new PC(20, 2, 0), new PC(21, 2, 0), new PC(22, 2, 0), new PC(23, 2, 0), new PC(8, 2, 0), new PC(9, 2, 0), new PC(10, 2, 0), new PC(11, 2, 0), new PC(12, 2, 0), new PC(13, 2, 0), new PC(14, 2, 0), new PC(15, 2, 0),
+                        new PC(0, 3, 0), new PC(1, 3, 0), new PC(2, 3, 0), new PC(3, 3, 0), new PC(4, 3, 0), new PC(5, 3, 0), new PC(6, 3, 0), new PC(7, 3, 0), new PC(16, 3, 0), new PC(17, 3, 0), new PC(18, 3, 0), new PC(19, 3, 0), new PC(20, 3, 0), new PC(21, 3, 0), new PC(22, 3, 0), new PC(23, 3, 0), new PC(8, 3, 0), new PC(9, 3, 0), new PC(10, 3, 0), new PC(11, 3, 0), new PC(12, 3, 0), new PC(13, 3, 0), new PC(14, 3, 0), new PC(15, 3, 0),
+                    },
+                    new[]
+                    {
+                         0,  1,  2,  3,  4,  5,  6,  7, 16, 17, 18, 19, 20, 21, 22, 23,  8,  9, 10, 11, 12, 13, 14, 15,
+                        24, 25, 26, 27, 28, 29, 30, 31, 40, 41, 42, 43, 44, 45, 46, 47, 32, 33, 34, 35, 36, 37, 38, 39,
+                        48, 49, 50, 51, 52, 53, 54, 55, 64, 65, 66, 67, 68, 69, 70, 71, 56, 57, 58, 59, 60, 61, 62, 63,
+                        72, 73, 74, 75, 76, 77, 78, 79, 88, 89, 90, 91, 92, 93, 94, 95, 80, 81, 82, 83, 84, 85, 86, 87
+                    }
                 );
 
                 yield return new TestCaseData
                 (
-                    new[] 
+                    new[]
                     { 
                         "AAAABBBBDDDDCCCC",
                         "CCCCDDDDAAAABBBB"
-                    }, 32,
-                    new[] 
+                    }, 16, 4, 2, 32,
+                    new[]
                     {
-                         0,   1,  2,  3,  8,  9, 10, 11, 24, 25, 26, 27, 16, 17, 18, 19,
-                         20, 21, 22, 23, 28, 29, 30, 31,  4,  5,  6,  7, 12, 13, 14, 15
+                        // Map from bit index 0...127
+                        new PC(0, 0, 0), new PC(1, 0, 0), new PC(2, 0, 0), new PC(3, 0, 0), new PC(8, 0, 1), new PC(9, 0, 1), new PC(10, 0, 1), new PC(11, 0, 1), new PC(4, 0, 0), new PC(5, 0, 0), new PC(6, 0, 0), new PC(7, 0, 0), new PC(12, 0, 1), new PC(13, 0, 1), new PC(14, 0, 1), new PC(15, 0, 1),
+                        new PC(12, 0, 0), new PC(13, 0, 0), new PC(14, 0, 0), new PC(15, 0, 0), new PC(0, 0, 1), new PC(1, 0, 1), new PC(2, 0, 1), new PC(3, 0, 1), new PC(8, 0, 0), new PC(9, 0, 0), new PC(10, 0, 0), new PC(11, 0, 0), new PC(4, 0, 1), new PC(5, 0, 1), new PC(6, 0, 1), new PC(7, 0, 1),
+
+                        new PC(0, 1, 0), new PC(1, 1, 0), new PC(2, 1, 0), new PC(3, 1, 0), new PC(8, 1, 1), new PC(9, 1, 1), new PC(10, 1, 1), new PC(11, 1, 1), new PC(4, 1, 0), new PC(5, 1, 0), new PC(6, 1, 0), new PC(7, 1, 0), new PC(12, 1, 1), new PC(13, 1, 1), new PC(14, 1, 1), new PC(15, 1, 1),
+                        new PC(12, 1, 0), new PC(13, 1, 0), new PC(14, 1, 0), new PC(15, 1, 0), new PC(0, 1, 1), new PC(1, 1, 1), new PC(2, 1, 1), new PC(3, 1, 1), new PC(8, 1, 0), new PC(9, 1, 0), new PC(10, 1, 0), new PC(11, 1, 0), new PC(4, 1, 1), new PC(5, 1, 1), new PC(6, 1, 1), new PC(7, 1, 1),
+
+                        new PC(0, 2, 0), new PC(1, 2, 0), new PC(2, 2, 0), new PC(3, 2, 0), new PC(8, 2, 1), new PC(9, 2, 1), new PC(10, 2, 1), new PC(11, 2, 1), new PC(4, 2, 0), new PC(5, 2, 0), new PC(6, 2, 0), new PC(7, 2, 0), new PC(12, 2, 1), new PC(13, 2, 1), new PC(14, 2, 1), new PC(15, 2, 1),
+                        new PC(12, 2, 0), new PC(13, 2, 0), new PC(14, 2, 0), new PC(15, 2, 0), new PC(0, 2, 1), new PC(1, 2, 1), new PC(2, 2, 1), new PC(3, 2, 1), new PC(8, 2, 0), new PC(9, 2, 0), new PC(10, 2, 0), new PC(11, 2, 0), new PC(4, 2, 1), new PC(5, 2, 1), new PC(6, 2, 1), new PC(7, 2, 1),
+
+                        new PC(0, 3, 0), new PC(1, 3, 0), new PC(2, 3, 0), new PC(3, 3, 0), new PC(8, 3, 1), new PC(9, 3, 1), new PC(10, 3, 1), new PC(11, 3, 1), new PC(4, 3, 0), new PC(5, 3, 0), new PC(6, 3, 0), new PC(7, 3, 0), new PC(12, 3, 1), new PC(13, 3, 1), new PC(14, 3, 1), new PC(15, 3, 1),
+                        new PC(12, 3, 0), new PC(13, 3, 0), new PC(14, 3, 0), new PC(15, 3, 0), new PC(0, 3, 1), new PC(1, 3, 1), new PC(2, 3, 1), new PC(3, 3, 1), new PC(8, 3, 0), new PC(9, 3, 0), new PC(10, 3, 0), new PC(11, 3, 0), new PC(4, 3, 1), new PC(5, 3, 1), new PC(6, 3, 1), new PC(7, 3, 1),
+                    },
+                    new[]
+                    {
+                        // Map from PlaneCoordinate(0, 0, 0) to PlaneCoordinate(15, 3, 1) in x, then y, then plane incrementing order
+                        // Plane 0
+                         0,   1,   2,   3,   8,   9,  10,  11,  24,  25,  26,  27,  16,  17,  18,  19,
+                        32,  33,  34,  35,  40,  41,  42,  43,  56,  57,  58,  59,  48,  49,  50,  51,
+                        64,  65,  66,  67,  72,  73,  74,  75,  88,  89,  90,  91,  80,  81,  82,  83,
+                        96,  97,  98,  99, 104, 105, 106, 107, 120, 121, 122, 123, 112, 113, 114, 115,
+
+                        // Plane 1
+                         20,  21,  22,  23,  28,  29,  30,  31,   4,   5,   6,   7,  12,  13,  14,  15,
+                         52,  53,  54,  55,  60,  61,  62,  63,  36,  37,  38,  39,  44,  45,  46,  47,
+                         84,  85,  86,  87,  92,  93,  94,  95,  68,  69,  70,  71,  76,  77,  78,  79,
+                        116, 117, 118, 119, 124, 125, 126, 127, 100, 101, 102, 103, 108, 109, 110, 111
+                    }
+                );
+
+                yield return new TestCaseData
+                (
+                    new[]
+                    {
+                        "AABBCCDD",
+                        "AABBCCDD",
+                        "AABBCCDD",
+                        "AABBCCDD"
+                    }, 8, 2, 4, 32,
+                    new[]
+                    {
+                        new PC(0, 0, 0), new PC(1, 0, 0), new PC(0, 0, 1), new PC(1, 0, 1), new PC(0, 0, 2), new PC(1, 0, 2), new PC(0, 0, 3), new PC(1, 0, 3), new PC(2, 0, 0), new PC(3, 0, 0), new PC(2, 0, 1), new PC(3, 0, 1), new PC(2, 0, 2), new PC(3, 0, 2), new PC(2, 0, 3), new PC(3, 0, 3),
+                        new PC(4, 0, 0), new PC(5, 0, 0), new PC(4, 0, 1), new PC(5, 0, 1), new PC(4, 0, 2), new PC(5, 0, 2), new PC(4, 0, 3), new PC(5, 0, 3), new PC(6, 0, 0), new PC(7, 0, 0), new PC(6, 0, 1), new PC(7, 0, 1), new PC(6, 0, 2), new PC(7, 0, 2), new PC(6, 0, 3), new PC(7, 0, 3),
+
+                        new PC(0, 1, 0), new PC(1, 1, 0), new PC(0, 1, 1), new PC(1, 1, 1), new PC(0, 1, 2), new PC(1, 1, 2), new PC(0, 1, 3), new PC(1, 1, 3), new PC(2, 1, 0), new PC(3, 1, 0), new PC(2, 1, 1), new PC(3, 1, 1), new PC(2, 1, 2), new PC(3, 1, 2), new PC(2, 1, 3), new PC(3, 1, 3),
+                        new PC(4, 1, 0), new PC(5, 1, 0), new PC(4, 1, 1), new PC(5, 1, 1), new PC(4, 1, 2), new PC(5, 1, 2), new PC(4, 1, 3), new PC(5, 1, 3), new PC(6, 1, 0), new PC(7, 1, 0), new PC(6, 1, 1), new PC(7, 1, 1), new PC(6, 1, 2), new PC(7, 1, 2), new PC(6, 1, 3), new PC(7, 1, 3)
+                    },
+                    new[]
+                    {
+                        // Map from PlaneCoordinate(0, 0, 0) to PlaneCoordinate(7, 1, 3) in x, then y, then plane incrementing order
+                        // Plane 0
+                         0,  1,  8,  9, 16, 17, 24, 25,
+                        32, 33, 40, 41, 48, 49, 56, 57,
+
+                        // Plane 1
+                         2,  3, 10, 11, 18, 19, 26, 27,
+                        34, 35, 42, 43, 50, 51, 58, 59,
+
+                        // Plane 2
+                         4,  5, 12, 13, 20, 21, 28, 29,
+                        36, 37, 44, 45, 52, 53, 60, 61,
+
+                        // Plane 3
+                         6,  7, 14, 15, 22, 23, 30, 31,
+                        38, 39, 46, 47, 54, 55, 62, 63
+                    }
+                );
+            }
+        }
+
+        public static IEnumerable<TestCaseData> TryCreateRemapChunkyPatternTestCases
+        {
+            get
+            {
+                yield return new TestCaseData
+                (
+                    new[]
+                    {
+                        "AABBCCDD",
+                    }, 8, 2, 4, 32,
+                    new[]
+                    {
+                        new PC(0, 0, 0), new PC(0, 0, 1), new PC(0, 0, 2), new PC(0, 0, 3), new PC(1, 0, 0), new PC(1, 0, 1), new PC(1, 0, 2), new PC(1, 0, 3), new PC(2, 0, 0), new PC(2, 0, 1), new PC(2, 0, 2), new PC(2, 0, 3), new PC(3, 0, 0), new PC(3, 0, 1), new PC(3, 0, 2), new PC(3, 0, 3), new PC(4, 0, 0), new PC(4, 0, 1), new PC(4, 0, 2), new PC(4, 0, 3), new PC(5, 0, 0), new PC(5, 0, 1), new PC(5, 0, 2), new PC(5, 0, 3), new PC(6, 0, 0), new PC(6, 0, 1), new PC(6, 0, 2), new PC(6, 0, 3), new PC(7, 0, 0), new PC(7, 0, 1), new PC(7, 0, 2), new PC(7, 0, 3),
+                        new PC(0, 1, 0), new PC(0, 1, 1), new PC(0, 1, 2), new PC(0, 1, 3), new PC(1, 1, 0), new PC(1, 1, 1), new PC(1, 1, 2), new PC(1, 1, 3), new PC(2, 1, 0), new PC(2, 1, 1), new PC(2, 1, 2), new PC(2, 1, 3), new PC(3, 1, 0), new PC(3, 1, 1), new PC(3, 1, 2), new PC(3, 1, 3), new PC(4, 1, 0), new PC(4, 1, 1), new PC(4, 1, 2), new PC(4, 1, 3), new PC(5, 1, 0), new PC(5, 1, 1), new PC(5, 1, 2), new PC(5, 1, 3), new PC(6, 1, 0), new PC(6, 1, 1), new PC(6, 1, 2), new PC(6, 1, 3), new PC(7, 1, 0), new PC(7, 1, 1), new PC(7, 1, 2), new PC(7, 1, 3),
+
+                    },
+                    new[]
+                    {
+                        // Map from PlaneCoordinate(0, 0, 0) to PlaneCoordinate(7, 1, 3) in plane, then x, then y incrementing order
+                        // Row 1
+                         0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+
+                         //Row 2
+                        32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63
                     }
                 );
             }
@@ -37,7 +145,7 @@ namespace ImageMagitek.Codec.UnitTests
             {
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAACCCCCCCCCBBBBBBB" }, 24
+                    new[] { "AAAAAAAACCCCCCCCCBBBBBBB" }, 24, 8, 1, 24
                 );
             }
         }
@@ -48,12 +156,12 @@ namespace ImageMagitek.Codec.UnitTests
             {
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAACCCCCCCCCBBBBBBBB" }, 23
+                    new[] { "AAAAAAAACCCCCCCCCBBBBBBBB" }, 24, 8, 1, 23
                 );
 
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAACCCCCCCCCBBBBBBBBD" }, 24
+                    new[] { "AAAAAAAACCCCCCCCCBBBBBBBBD" }, 24, 8, 1, 23
                 );
             }
         }
@@ -64,12 +172,12 @@ namespace ImageMagitek.Codec.UnitTests
             {
                 yield return new TestCaseData
                 (
-                    new[] { "" }, 0
+                    new[] { "" }, 8, 8, 1, 0
                 );
 
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAA" }, -1
+                    new[] { "AAAAAAAA" }, 8, 8, 1, -1
                 );
             }
         }
@@ -80,12 +188,12 @@ namespace ImageMagitek.Codec.UnitTests
             {
                 yield return new TestCaseData
                 (
-                    new[] { "AAAAAAAACCCCCCCC00000000" }, 24
+                    new[] { "AAAAAAAACCCCCCCC00000000" }, 24, 8, 1, 24
                 );
 
                 yield return new TestCaseData
                 (
-                    new[] { "11111111CCCCCCCC,,,,,,,," }, 24
+                    new[] { "11111111CCCCCCCC,,,,,,,," }, 24, 8, 1, 24
                 );
             }
         }
--- ImageMagitek.UnitTests/PatternListTests.cs
+++ ImageMagitek.UnitTests/PatternListTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using MoreLinq;
 using NUnit.Framework;
 
 namespace ImageMagitek.Codec.UnitTests
@@ -7,20 +8,90 @@ namespace ImageMagitek.Codec.UnitTests
     [TestFixture]
     public class PatternListTests
     {
-        [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPatternTestCases")]
-        public void TryCreateRemapPattern_AsExpected(IList<string> patterns, int size, IList<int> expected)
+        [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPlanarPatternTestCases")]
+        public void TryCreateRemapPattern_DecodePlanar_AsExpected(IList<string> patterns, int width, int height,
+            int planes, int size, IList<PlaneCoordinate> expectedDecoded, IList<int> expectedEncoded)
         {
-            var result = PatternList.TryCreateRemapPattern(patterns, size);
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
 
             result.Switch(
-                success => CollectionAssert.AreEqual(expected, success.Result),
+                success =>
+                {
+                    var actual = Enumerable.Range(0, width * height * planes)
+                        .Select(x => success.Result.GetDecodeIndex(x))
+                        .ToArray();
+
+                    CollectionAssert.AreEqual(expectedDecoded, actual);
+                },
+                failed => Assert.Fail(failed.Reason));
+        }
+
+        [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPlanarPatternTestCases")]
+        public void TryCreateRemapPattern_EncodePlanar_AsExpected(IList<string> patterns, int width, int height,
+            int planes, int size, IList<PlaneCoordinate> expectedDecoded, IList<int> expectedEncoded)
+        {
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
+
+            result.Switch(
+                success =>
+                {
+                    var actual = Enumerable.Range(0, planes)
+                        .Cartesian(
+                            Enumerable.Range(0, height),
+                            Enumerable.Range(0, width),
+                            (p, y, x) => new PlaneCoordinate((short)x, (short)y, (short)p))
+                        .Select(x => success.Result.GetEncodeIndex(x))
+                        .ToArray();
+
+                    CollectionAssert.AreEqual(expectedEncoded, actual);
+                },
+                failed => Assert.Fail(failed.Reason));
+        }
+
+        [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapChunkyPatternTestCases")]
+        public void TryCreateRemapPattern_DecodeChunky_AsExpected(IList<string> patterns, int width, int height,
+            int planes, int size, IList<PlaneCoordinate> expectedDecoded, IList<int> expectedEncoded)
+        {
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Chunky, width, height, planes, size);
+
+            result.Switch(
+                success =>
+                {
+                    var actual = Enumerable.Range(0, width * height * planes)
+                        .Select(x => success.Result.GetDecodeIndex(x))
+                        .ToArray();
+
+                    CollectionAssert.AreEqual(expectedDecoded, actual);
+                },
+                failed => Assert.Fail(failed.Reason));
+        }
+
+        [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapChunkyPatternTestCases")]
+        public void TryCreateRemapPattern_EncodeChunky_AsExpected(IList<string> patterns, int width, int height,
+            int planes, int size, IList<PlaneCoordinate> expectedDecoded, IList<int> expectedEncoded)
+        {
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Chunky, width, height, planes, size);
+
+            result.Switch(
+                success =>
+                {
+                    var actual = Enumerable.Range(0, height)
+                        .Cartesian(
+                            Enumerable.Range(0, width),
+                            Enumerable.Range(0, planes),
+                            (y, x, p) => new PlaneCoordinate((short)x, (short)y, (short)p))
+                        .Select(x => success.Result.GetEncodeIndex(x))
+                        .ToArray();
+
+                    CollectionAssert.AreEqual(expectedEncoded, actual);
+                },
                 failed => Assert.Fail(failed.Reason));
         }
 
         [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPatternTooManyLettersTestCases")]
-        public void TryCreateRemapPattern_TooManyLetters_Fails(IList<string> patterns, int size)
+        public void TryCreateRemapPattern_TooManyLetters_Fails(IList<string> patterns, int width, int height, int planes, int size)
         {
-            var result = PatternList.TryCreateRemapPattern(patterns, size);
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
 
             result.Switch(
                 success => Assert.Fail("TryCreateRemapPattern should have failed, but succeeded"),
@@ -28,9 +99,9 @@ namespace ImageMagitek.Codec.UnitTests
         }
 
         [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPatternOutOfRangeTestCases")]
-        public void TryCreateRemapPattern_OutOfRange_Fails(IList<string> patterns, int size)
+        public void TryCreateRemapPattern_OutOfRange_Fails(IList<string> patterns, int width, int height, int planes, int size)
         {
-            var result = PatternList.TryCreateRemapPattern(patterns, size);
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
 
             result.Switch(
                 success => Assert.Fail("TryCreateRemapPattern should have failed, but succeeded"),
@@ -38,9 +109,9 @@ namespace ImageMagitek.Codec.UnitTests
         }
 
         [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPatternInvalidSizeTestCases")]
-        public void TryCreateRemapPattern_InvalidSize_Fails(IList<string> patterns, int size)
+        public void TryCreateRemapPattern_InvalidSize_Fails(IList<string> patterns, int width, int height, int planes, int size)
         {
-            var result = PatternList.TryCreateRemapPattern(patterns, size);
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
 
             result.Switch(
                 success => Assert.Fail("TryCreateRemapPattern should have failed, but succeeded"),
@@ -48,9 +119,9 @@ namespace ImageMagitek.Codec.UnitTests
         }
 
         [TestCaseSource(typeof(PatternListTestCases), "TryCreateRemapPatternInvalidCharacterTestCases")]
-        public void TryCreateRemapPattern_InvalidCharacter_Fails(IList<string> patterns, int size)
+        public void TryCreateRemapPattern_InvalidCharacter_Fails(IList<string> patterns, int width, int height, int planes, int size)
         {
-            var result = PatternList.TryCreateRemapPattern(patterns, size);
+            var result = PatternList.TryCreatePatternList(patterns, PixelPacking.Planar, width, height, planes, size);
 
             result.Switch(
                 success => Assert.Fail("TryCreateRemapPattern should have failed, but succeeded"),
