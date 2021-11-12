9ac08ff9e381c07573c4133dee7f3dacb56edaeb
Stephen Monaco <stevemonaco@hotmail.com>
2021-11-12 12:22:51 -0500

    Added ArrangerBuilder

---
+++ ImageMagitek.UnitTests/BuilderTests/ArrangerBuilderTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using System.IO;
+using ImageMagitek.Builders;
+using ImageMagitek.Codec;
+using NUnit.Framework;
+
+namespace ImageMagitek.UnitTests.BuilderTests;
+
+[TestFixture]
+public class ArrangerBuilderTests
+{
+    [Test]
+    public void Build_ScatteredArranger()
+    {
+        ScatteredArranger arranger = ArrangerBuilder
+            .WithTiledLayout()
+            .WithArrangerElementSize(8, 16)
+            .WithElementPixelSize(8, 8)
+            .WithPixelColorType(PixelColorType.Indexed)
+            .WithName("ScatteredTestArranger")
+            .AsScatteredArranger()
+            .Build();
+
+        Assert.That(arranger.Layout == ElementLayout.Tiled);
+        Assert.That(arranger.ArrangerElementSize == new Size(8, 16));
+        Assert.That(arranger.ElementPixelSize == new Size(8, 8));
+        Assert.That(arranger.ColorType == PixelColorType.Indexed);
+        Assert.That(arranger.Name == "ScatteredTestArranger");
+    }
+
+    [Test]
+    public void Build_SequentialArranger()
+    {
+        var codecFactory = new CodecFactory(new());
+        var dataFile = new DataFile("TestMemoryFile", new MemoryStream(2 * 256 * 512));
+
+        SequentialArranger arranger = ArrangerBuilder
+            .WithSingleLayout()
+            .WithElementPixelSize(256, 512)
+            .WithPixelColorType(PixelColorType.Direct)
+            .WithName("SequentialTestArranger")
+            .AsSequentialArranger(codecFactory)
+            .WithDataFile(dataFile)
+            .WithCodecName("PSX 16bpp")
+            .Build();
+
+        Assert.That(arranger.Layout == ElementLayout.Single);
+        Assert.That(arranger.ArrangerElementSize == new Size(1, 1));
+        Assert.That(arranger.ElementPixelSize == new Size(256, 512));
+        Assert.That(arranger.ColorType == PixelColorType.Direct);
+        Assert.That(arranger.Name == "SequentialTestArranger");
+        Assert.That(arranger.ActiveCodec.Name == "PSX 16bpp");
+        Assert.That(arranger.ActiveCodec.ColorType == PixelColorType.Direct);
+        Assert.That(arranger.ActiveCodec.Layout == ImageLayout.Single);
+        Assert.That(arranger.ActiveCodec.Width == 256);
+        Assert.That(arranger.ActiveCodec.Height == 512);
+        Assert.That(arranger.ActivePalette is null);
+        Assert.That(arranger.ActiveDataFile.Name == "TestMemoryFile");
+    }
+
+}
---
+++ ImageMagitek/Builders/ArrangerBuilder.cs
@@ -0,0 +1,162 @@
+﻿using System.Drawing;
+using ImageMagitek.Codec;
+using ImageMagitek.Colors;
+
+namespace ImageMagitek.Builders;
+
+public interface IArrangerBuilderArrangerElementSize
+{
+    IArrangerBuilderElementPixelSize WithArrangerElementSize(int x, int y);
+}
+
+public interface IArrangerBuilderElementPixelSize
+{
+    IArrangerBuilderPixelColorType WithElementPixelSize(int x, int y);
+}
+
+public interface IArrangerBuilderPixelColorType
+{
+    IArrangerBuilderName WithPixelColorType(PixelColorType pixelColorType);
+}
+
+public interface IArrangerBuilderName
+{
+    IArrangerBuilderType WithName(string name);
+}
+
+public interface IArrangerBuilderType
+{
+    ISequentialArrangerBuilderSources AsSequentialArranger(ICodecFactory codecFactory);
+    IScatteredArrangerBuilder AsScatteredArranger();
+}
+
+public interface ISequentialArrangerBuilderSources
+{
+    ISequentialArrangerBuilderSources WithDataFile(DataFile df);
+    ISequentialArrangerBuilderSources WithCodecName(string codecName);
+    ISequentialArrangerBuilderSources WithPalette(Palette palette);
+    ISequentialArrangerBuilderSources WithElementLayout(ElementLayout layout);
+    SequentialArranger Build();
+}
+
+public interface IScatteredArrangerBuilder
+{
+    ScatteredArranger Build();
+}
+
+public class ArrangerBuilder :
+    IArrangerBuilderArrangerElementSize,
+    IArrangerBuilderElementPixelSize,
+    IArrangerBuilderPixelColorType,
+    IArrangerBuilderName,
+    IArrangerBuilderType,
+    ISequentialArrangerBuilderSources,
+    IScatteredArrangerBuilder
+{
+    private ArrangerMode _mode;
+    private ICodecFactory _codecFactory;
+    private PixelColorType _pixelColorType;
+    private ElementLayout _elementLayout;
+    private Size _arrangerElementSize;
+    private Size _elementPixelSize;
+    private string _name;
+    private DataFile _dataFile;
+    private string _codecName;
+    private Palette _palette;
+
+    private ArrangerBuilder()
+    {
+    }
+
+    public static IArrangerBuilderElementPixelSize WithSingleLayout()
+    {
+        var builder = new ArrangerBuilder();
+        builder._elementLayout = ElementLayout.Single;
+        builder._arrangerElementSize = new(1, 1);
+        return builder;
+    }
+
+    public static IArrangerBuilderArrangerElementSize WithTiledLayout()
+    {
+        var builder = new ArrangerBuilder();
+        builder._elementLayout = ElementLayout.Tiled;
+        return builder;
+    }
+
+    public IArrangerBuilderElementPixelSize WithArrangerElementSize(int x, int y)
+    {
+        _arrangerElementSize = new(x, y);
+        return this;
+    }
+
+    public IArrangerBuilderPixelColorType WithElementPixelSize(int x, int y)
+    {
+        _elementPixelSize = new(x, y);
+        return this;
+    }
+
+    public IArrangerBuilderName WithPixelColorType(PixelColorType pixelColorType)
+    {
+        _pixelColorType = pixelColorType;
+        return this;
+    }
+
+    public IArrangerBuilderType WithName(string name)
+    {
+        _name = name;
+        return this;
+    }
+
+    public ISequentialArrangerBuilderSources AsSequentialArranger(ICodecFactory codecFactory)
+    {
+        _mode = ArrangerMode.Sequential;
+        _codecFactory = codecFactory;
+
+        return this;
+    }
+
+    public IScatteredArrangerBuilder AsScatteredArranger()
+    {
+        _mode = ArrangerMode.Scattered;
+
+        return this;
+    }
+
+    public ISequentialArrangerBuilderSources WithDataFile(DataFile dataFile)
+    {
+        _dataFile = dataFile;
+        return this;
+    }
+
+    public ISequentialArrangerBuilderSources WithCodecName(string codecName)
+    {
+        _codecName = codecName;
+        return this;
+    }
+
+    public ISequentialArrangerBuilderSources WithPalette(Palette palette)
+    {
+        _palette = palette;
+        return this;
+    }
+
+    public ISequentialArrangerBuilderSources WithElementLayout(ElementLayout elementLayout)
+    {
+        _elementLayout = elementLayout;
+        return this;
+    }
+
+    SequentialArranger ISequentialArrangerBuilderSources.Build()
+    {
+        var arranger = new SequentialArranger(_arrangerElementSize.Width, _arrangerElementSize.Height, _dataFile, _palette, _codecFactory, _codecName);
+        arranger.Name = _name;
+        var codec = _codecFactory.GetCodec(arranger.ActiveCodec.Name, _elementPixelSize);
+        arranger.ChangeCodec(codec);
+        return arranger;
+    }
+
+    ScatteredArranger IScatteredArrangerBuilder.Build()
+    {
+        return new ScatteredArranger(_name, _pixelColorType, _elementLayout, _arrangerElementSize.Width, _arrangerElementSize.Height, _elementPixelSize.Width, _elementPixelSize.Height);
+    }
+}
