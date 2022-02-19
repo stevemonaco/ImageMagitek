61ba75be256779414b7908251aba6c35c68e2e46
Stephen Monaco <stevemonaco@hotmail.com>
2022-02-19 11:47:15 -0500

    Reimplement exploring for Win platform

--- TileShop.Avalonia/Services/DiskExploreService.cs
+++ TileShop.Avalonia/Services/DiskExploreService.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 using TileShop.Shared.Services;
 
 namespace TileShop.AvaloniaUI.Services;
 
 internal class DiskExploreService : IDiskExploreService
 {
-    public void ExploreDiskLocation(string location) => throw new NotImplementedException();
+    public void ExploreDiskLocation(string location)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            string command = $"explorer.exe";
+            string args = $"/select, {location}";
+            Process.Start(command, args);
+        }
+    }
 }
