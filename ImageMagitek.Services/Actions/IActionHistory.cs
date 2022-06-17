d7772fb810ea9bc6a97c5ecd69a50f63d770a1a8
Stephen Monaco <stevemonaco@hotmail.com>
2022-06-17 08:31:01 -0400

    Introduce history actions

---
+++ ImageMagitek.Services/Actions/IActionHistory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageMagitek.Services.Actions;
+
+public interface IActionHistory
+{
+    void Undo();
+    void Redo();
+}
---
+++ ImageMagitek.Services/Actions/MagitekActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using ImageMagitek.Colors;
+
+namespace ImageMagitek.Services.Actions;
+
+public interface IMagitekAction
+{
+}
+
+public record PencilAction<TColor>(TColor PencilColor, IReadOnlyCollection<Point> ModifiedPoints) : IMagitekAction
+    where TColor : struct;
+
+public record FloodFillTestAction<TColor>(TColor FillColor, int X, int Y) : IMagitekAction
+    where TColor : struct;
+
+public record ApplyPaletteAction(Palette Palette, IReadOnlyCollection<Point> ModifiedElements);
+public record DeleteElementSelectionAction(Rectangle Rect) : IMagitekAction;
+public record DeleteElementAction(int ElementX, int ElementY) : IMagitekAction;
+
+public record ResizeArrangerAction(int Width, int Height) : IMagitekAction;
+public record MirrorElementAction(int ElementX, int ElementY, MirrorOperation Mirror) : IMagitekAction;
+public record RotateElementAction(int ElementX, int ElementY, RotationOperation Rotation) : IMagitekAction;
