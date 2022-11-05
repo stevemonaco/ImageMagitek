cff4834bd6804dce16359f1d299516e9b0873104
Stephen Monaco <stevemonaco@hotmail.com>
2022-11-05 05:38:22 -0400

    Add key support

--- TileShop.Avalonia/ViewExtenders/Input/InputAdapter.cs
+++ TileShop.Avalonia/ViewExtenders/Input/InputAdapter.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Input;
+﻿using System.Collections.Generic;
+using Avalonia.Input;
 using TileShop.Shared.Input;
 
 using KeyModifiers = TileShop.Shared.Input.KeyModifiers;
+using Key = TileShop.Shared.Input.Key;
+using AvKey = Avalonia.Input.Key;
 
 namespace TileShop.AvaloniaUI.Input;
 public static class InputAdapter
@@ -12,6 +15,25 @@ public static class InputAdapter
         var modifiers = CreateKeyModifiers(keys);
 
         return new MouseState(properties.IsLeftButtonPressed, properties.IsMiddleButtonPressed, properties.IsRightButtonPressed, modifiers);
+    }
+
+    public static KeyState CreateKeyState(Avalonia.Input.Key inputKey, Avalonia.Input.KeyModifiers keyModifiers)
+    {
+        var modifiers = CreateKeyModifiers(keyModifiers);
+
+        if (_keyMap.TryGetValue(inputKey, out var key))
+        {
+            if (modifiers.HasFlag(KeyModifiers.Shift) && _shiftKeyMap.TryGetValue(key, out var shiftKey))
+            {
+                return new KeyState(shiftKey, modifiers);
+            }
+            else
+            {
+                return new KeyState(key, modifiers);
+            }
+        }
+
+        return new KeyState(Key.None, modifiers);
     }
 
     public static KeyModifiers CreateKeyModifiers(Avalonia.Input.KeyModifiers keys)
@@ -28,4 +50,33 @@ public static class InputAdapter
 
         return modifiers;
     }
+
+    private static Dictionary<AvKey, Key> _keyMap = new()
+    {
+        [AvKey.A] = Key.A, [AvKey.B] = Key.B, [AvKey.C] = Key.C, [AvKey.D] = Key.D, [AvKey.E] = Key.E,
+        [AvKey.F] = Key.F, [AvKey.G] = Key.G, [AvKey.H] = Key.H, [AvKey.I] = Key.I, [AvKey.J] = Key.J,
+        [AvKey.K] = Key.K, [AvKey.L] = Key.L, [AvKey.M] = Key.M, [AvKey.N] = Key.N, [AvKey.O] = Key.O,
+        [AvKey.P] = Key.P, [AvKey.Q] = Key.Q, [AvKey.R] = Key.R, [AvKey.S] = Key.S, [AvKey.T] = Key.T,
+        [AvKey.U] = Key.U, [AvKey.V] = Key.V, [AvKey.W] = Key.W, [AvKey.X] = Key.X, [AvKey.Y] = Key.Y,
+        [AvKey.Z] = Key.Z, [AvKey.D0] = Key.Digit0, [AvKey.D1] = Key.Digit1, [AvKey.D2] = Key.Digit2, [AvKey.D3] = Key.Digit3,
+        [AvKey.D4] = Key.Digit4, [AvKey.D5] = Key.Digit5, [AvKey.D6] = Key.Digit6, [AvKey.D7] = Key.Digit7, [AvKey.D8] = Key.Digit8,
+        [AvKey.D9] = Key.Digit9, [AvKey.OemPeriod] = Key.Period, [AvKey.OemComma] = Key.Comma, [AvKey.OemQuestion] = Key.ForwardSlash, [AvKey.OemTilde] = Key.Backtick,
+        [AvKey.OemMinus] = Key.Minus, [AvKey.OemPlus] = Key.Equal, [AvKey.Back] = Key.Backspace, [AvKey.OemOpenBrackets] = Key.OpenBracket, [AvKey.OemCloseBrackets] = Key.CloseBracket,
+        [AvKey.OemBackslash] = Key.BackSlash, [AvKey.OemPipe] = Key.BackSlash, [AvKey.OemSemicolon] = Key.Semicolon, [AvKey.OemQuotes] = Key.Apostrophe,
+        [AvKey.Delete] = Key.Delete, [AvKey.Home] = Key.Home, [AvKey.End] = Key.End, [AvKey.PageUp] = Key.PageUp, [AvKey.PageDown] = Key.PageDown,
+        [AvKey.Up] = Key.Up, [AvKey.Down] = Key.Down, [AvKey.Left] = Key.Left, [AvKey.Right] = Key.Right, [AvKey.Tab] = Key.Tab,
+        [AvKey.LeftShift] = Key.LeftShift, [AvKey.RightShift] = Key.RightShift, [AvKey.LeftCtrl] = Key.LeftControl, [AvKey.RightCtrl] = Key.RightControl, [AvKey.LeftAlt] = Key.LeftAlt,
+        [AvKey.RightAlt] = Key.RightAlt, [AvKey.Enter] = Key.Enter, [AvKey.Space] = Key.Space
+    };
+
+    private static Dictionary<Key, Key> _shiftKeyMap = new()
+    {
+        [Key.Digit1] = Key.Exclamation, [Key.Digit2]  = Key.At, [Key.Digit3] = Key.Hash,
+        [Key.Digit4] = Key.Dollar, [Key.Digit5] = Key.Percent, [Key.Digit6] = Key.Carat,
+        [Key.Digit7] = Key.Ampersand, [Key.Digit8] = Key.Star, [Key.Digit9] = Key.OpenParenthesis,
+        [Key.Digit0] = Key.CloseParenthesis, [Key.Minus] = Key.Underscore, [Key.Equal] = Key.Plus,
+        [Key.Backtick] = Key.Tilde, [Key.BackSlash] = Key.Pipe, [Key.Period]  = Key.GreaterThan,
+        [Key.Comma] = Key.LessThan, [Key.ForwardSlash] = Key.Question, [Key.OpenBracket] = Key.OpenBrace,
+        [Key.CloseBracket] = Key.CloseBrace
+    };
 }
--- TileShop.Shared/Input/Key.cs
+++ TileShop.Shared/Input/Key.cs
@@ -4,8 +4,12 @@ public enum Key
 {
     A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
     Digit0, Digit1, Digit2, Digit3, Digit4, Digit5, Digit6, Digit7, Digit8, Digit9,
-    Period, Comma, Question, 
+    Period, Comma, Question, Tab,
     Backtick, Tilde, Exclamation, At, Hash, Dollar, Percent, Carat, Ampersand, Star, OpenParenthesis, CloseParenthesis, Minus, Underscore, Plus, Equal, Backspace,
     OpenBracket, CloseBracket, OpenBrace, CloseBrace, BackSlash, Pipe,
-    Semicolon, Colon, Apostrophe, DoubleQuote
+    LessThan, GreaterThan, ForwardSlash,
+    Delete, Home, End, PageUp, PageDown, Up, Down, Left, Right,
+    Semicolon, Colon, Apostrophe, DoubleQuote,
+    LeftAlt, RightAlt, LeftControl, RightControl, LeftShift, RightShift, Enter, Space,
+    None
 }
