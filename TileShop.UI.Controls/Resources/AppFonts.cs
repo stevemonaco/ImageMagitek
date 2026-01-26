3837a6aa79aff57a70b4a8251a2b24e9b3217620
Steve Monaco <1782158+stevemonaco@users.noreply.github.com>
2026-01-26 00:04:50 -0500

    Resource migration, add new font, cleanup dialog visuals

---
+++ TileShop.UI.Controls/Resources/AppFonts.cs
@@ -0,0 +1,8 @@
+﻿using Avalonia.Media;
+
+namespace TileShop.UI.Controls;
+
+public static class AppFonts
+{
+    public static FontFamily IbmPlexSans { get; } = new FontFamily("avares://TileShop.UI.Controls/Assets/Fonts/IBM Plex Sans#IBM Plex Sans");
+}
